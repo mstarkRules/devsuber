4c6e2557dc85a346685388b71f11453cb5fc5c90
Marcos Amorim <markstm10@gmail.com>
2020-12-07 00:25:44 -0300

    modificado app.js e mainStack.js

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/es/integration/react';
 import {store, persistor} from './src/store';
 
 import MainStack from './src/navigators/MainStack';
 
-export default()=>(
+export default () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <MainStack />
--- src/navigators/MainStack.js
+++ src/navigators/MainStack.js
@@ -10,4 +10,9 @@ export default createAppContainer(createStackNavigator({
     Preload,
     Login,
    // HomeStack
+},{
+    initialRouteName:'Preload',
+    defaultNavigationOptions:{
+        header: null
+    }
 }));
