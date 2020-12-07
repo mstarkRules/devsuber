b5c913918900219adfd1d45f6711a23f96ddb66e
Marcos Amorim <markstm10@gmail.com>
2020-12-06 23:23:03 -0300

    configurado estrutura inicial do redux

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-teste
 Projeto realizado com o intuito de aprender bibliotecas e skills necessárias para aplicações que manipulem mapas e localização.
 
 Esse projeto é um aplicativo baseado no UBER, com fins educacionais.
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    name:''
+};
+
+export default(state = initialState, action) =>{
+
+    switch(action.type){
+        case 'SET_NAME':
+            return {...state, name:action.payload}
+            break;
+    }
+
+    return state;
+}
